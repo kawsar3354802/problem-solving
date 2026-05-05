755383e0beeb5928bad2244e817530d74722a3cd
Kawsar Ahmed Ohid <kawsar3354802@gmail.com>
2026-05-05 14:52:50 +0300

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a = input.nextInt();
+        System.out.println("Enter b: ");
+        int b = input.nextInt();
+        System.out.println("Enter operator: ");
+        char operator = input.next().charAt(0);
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            case '%':
+                System.out.println(a % b);
+                break;
+            default:
+                System.out.println("invalid operator");
+        }
+
+    }
+}
